7d5f4afee0a4290c9e2a65c051730c5d00514a11
Darrell Payne <darrellpayne@earthlink.net>
2022-03-08 23:07:19 -0800

    Initial Commit

---
+++ Reminders/RemindersApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindersApp.swift
+//  Reminders
+//
+//  Created by DARRELL A PAYNE on 3/8/22.
+//
+
+import SwiftUI
+
+@main
+struct RemindersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
