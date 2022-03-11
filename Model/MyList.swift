e00bb09430b7a12896b8515580c8bee393704fc8
Darrell Payne <darrellpayne@earthlink.net>
2022-03-10 23:08:00 -0800

    Completed part 24 of section 5

---
+++ Model/MyList.swift
@@ -0,0 +1,30 @@
+//
+//  MyList+CoreDataProperties.swift
+//
+//
+//  Created by DARRELL A PAYNE on 3/10/22.
+
+import Foundation
+import CoreData
+import AppKit
+
+@objc(MyList)
+class MyList: NSManagedObject {
+    
+}
+
+
+extension MyList {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<MyList> {
+        return NSFetchRequest<MyList>(entityName: "MyList")
+    }
+
+    @NSManaged public var color: NSColor?
+    @NSManaged public var name: String?
+
+}
+
+extension MyList : Identifiable {
+
+}
