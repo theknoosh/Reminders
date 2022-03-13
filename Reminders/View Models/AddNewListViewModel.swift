ca939fc1f37a466a14e2b377f08e5b7c5a3b5ab9
Darrell Payne <darrellpayne@earthlink.net>
2022-03-12 23:12:39 -0800

    Completed part 26 of section 5

---
+++ Reminders/View Models/AddNewListViewModel.swift
@@ -0,0 +1,29 @@
+//
+//  AddNewListViewModel.swift
+//  Reminders
+//
+//  Created by DARRELL A PAYNE on 3/12/22.
+//
+
+import Foundation
+import CoreData
+import SwiftUI
+
+class AddNewListViewModel: ObservableObject {
+    
+    @Published var name: String = ""
+    @Published var color: Color = .blue
+    
+    var context: NSManagedObjectContext
+    
+    init(context: NSManagedObjectContext) {
+        self.context = context
+    }
+    
+    func save() {
+        
+        let myList = MyList(context: context)
+        myList.name = name
+        myList.color = NSColor(color)
+    }
+}
