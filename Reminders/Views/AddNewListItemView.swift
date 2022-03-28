b7dab0c39ab3c21f00c46297cf4015995c4f9de7
Darrell Payne <darrellpayne@earthlink.net>
2022-03-27 17:16:29 -0700

    Completed part 37: Adding AddNewListView

---
+++ Reminders/Views/AddNewListItemView.swift
@@ -0,0 +1,49 @@
+//
+//  AddNewListItemView.swift
+//  Reminders
+//
+//  Created by DARRELL A PAYNE on 3/27/22.
+//
+
+import SwiftUI
+
+struct AddNewListItemView: View {
+    
+    @State private var title: String = ""
+    @State private var dueDate: DueDate?
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            HStack(alignment: .center) {
+                Image(systemName: Constants.Icons.circle)
+                    .font(.system(size: 14))
+                    .opacity(0.2)
+                TextField("", text: $title)
+            }
+            
+            Text("Notes")
+                .opacity(0.2)
+                .padding(.leading, 30)
+            
+            HStack {
+                DueDateSelectionView(dueDate: $dueDate)
+                
+                if dueDate != nil {
+                    Button("Clear") {
+                        dueDate = nil
+                    }
+                }
+                
+                Button("Save") {
+                    
+                }
+            }.padding()
+        }
+    }
+}
+
+struct AddNewListItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddNewListItemView()
+    }
+}
